dbc201649175e0225bfbae32a496b25e5ae87028
Marvelousy20 <afosmarvel@gmail.com>
2024-08-20 16:13:15 +0100

    wip

--- src/components/Common/Sidebar/SidebarIcon.tsx
+++ src/components/Common/Sidebar/SidebarIcon.tsx
@@ -1,10 +1,14 @@
 "use client";
-
+import React from "react";
 import { usePathname } from "next/navigation";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import Link from "next/link";
 import { ReactNode } from "react";
-
-
 
 interface SidebarIconProps {
   href: string;
@@ -26,18 +30,30 @@ const SidebarIcon: React.FC<SidebarIconProps> = ({
       : pathName === href || pathName.startsWith(`${href}/`);
 
   return (
-    <Link href={href}>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger>
+          <Link href={href}>
+            <div
+              className={`flex items-center justify-center p-1 rounded-[4px] mb-3 cursor-pointer ${
+                isActive
+                  ? "bg-primary p-1.5"
+                  : "bg-transparent hover:bg-[#CCCCCC0D]"
+              } hover:bg-gray-700`}
+              aria-label={altText}
+            >
+              {isActive ? activeIcon : inactiveIcon}
+            </div>
+          </Link>
+        </TooltipTrigger>
 
-      <div
-        className={`flex items-center justify-center p-1 rounded-[4px] mb-3  cursor-pointer ${
-          isActive ? "bg-primary p-1.5" : "bg-transparent hover:bg-[#CCCCCC0D]"
-        } hover:bg-gray-700`}
-        aria-label={altText}
-      >
-
-        {isActive ? activeIcon : inactiveIcon}
-      </div>
-    </Link>
+        <div className="tooltip-container">
+          <TooltipContent side="right" className="custom-tooltip">
+            <p className="bg-black py-2 px-3 rounded-[4px]">{altText}</p>
+          </TooltipContent>
+        </div>
+      </Tooltip>
+    </TooltipProvider>
   );
 };
 
