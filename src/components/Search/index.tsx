111a1b720dc1a324d718bed1bd2e8f7ddd3e9848
Divine-joy <akindivinejoy@gmail.com>
2024-08-28 14:39:39 -0700

    feat: search page

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -17,17 +17,14 @@ export default function Search() {
     { id: 6, name: "Status", img: "/main/search/account_box.svg" },
     { id: 7, name: "Topic", img: "/main/search/topic.svg" },
   ];
-const [search, setSearch] = useState<string>('');
-const handleCancel = () =>{
-  setSearch('')
-
-}
-
+  const [search, setSearch] = useState<string>("");
+  const handleCancel = () => {
+    setSearch("");
+  };
 
   const [openedItemId, setOpenedItemId] = useState<number | null>(null);
 
   const handleClick = (id: number) => {
-   
     if (id !== 1 && id !== 5) {
       setOpenedItemId(openedItemId === id ? null : id);
     }
@@ -56,14 +53,12 @@ const handleCancel = () =>{
         <input
           value={search}
           type="text"
-          onChange={(e)=> setSearch(e.target.value)}
+          onChange={(e) => setSearch(e.target.value)}
           placeholder="Search"
           className="pl-2 text-md text-inactive border border-gray-300 rounded-lg  w-full border-none focus:outline-none "
         />
-        <div
-        className="pr-[25px]"
-         onClick={handleCancel}>
-        <Image
+        <div className="pr-[25px]" onClick={handleCancel}>
+          <Image
             src="/main/search/close.svg"
             alt="search"
             width={20}
@@ -80,26 +75,16 @@ const handleCancel = () =>{
           {searchTools.map((item) => (
             <div
               key={item.id}
-              className={`flex gap-x-2 bg-gray-60 rounded-md p-[10px] ${
-                item.id === 3 || item.id === 6 ? "cursor-default" : "cursor-pointer"
-              }`}
+              className='flex gap-x-2 bg-gray-60 rounded-md p-[10px] cursor-pointer'
               onClick={() => handleClick(item.id)}
             >
-              <Image
-                src={item.img}
-                alt={item.name}
-                width={14}
-                height={14}
-              />
-              <p 
-              className={`text-[#101010] text-xs font-medium hover:text-primary  ${
-                openedItemId === item.id
-                  ? "text-primary"
-                  : "text-[#101010]"
-              }`}>
-                {openedItemId === item.id
-                  ? `${item.name}: cisco`
-                  : item.name}
+              <Image src={item.img} alt={item.name} width={14} height={14} />
+              <p
+                className={`text-[#101010] text-xs font-medium hover:text-primary  ${
+                  openedItemId === item.id ? "text-primary" : "text-[#101010]"
+                }`}
+              >
+                {openedItemId === item.id ? `${item.name}: cisco` : item.name}
               </p>
             </div>
           ))}
@@ -136,8 +121,8 @@ const handleCancel = () =>{
               Search conversations and tickets
             </p>
             <p className="text-inactive text-sm font-medium w-[354px] text-center">
-              Use relevant keywords, apply filters for precise results and
-              save frequent searches for quick access.
+              Use relevant keywords, apply filters for precise results and save
+              frequent searches for quick access.
             </p>
           </div>
         </div>
